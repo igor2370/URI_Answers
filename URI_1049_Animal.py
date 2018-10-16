424995abd883d3ca9998ff8253beaa4c875fb1dd
igor2370 <igor2370@hotmail.com>
2018-10-15 23:20:15 -0300

    Update URI_1049_Animal.py

---
+++ URI_1049_Animal.py
@@ -0,0 +1,8 @@
+l={'vertebrado':{'ave':{'carnivoro':'aguia','onivoro':'pomba'},
+                 'mamifero':{'onivoro':'homem','herbivoro':'vaca'}},
+   'invertebrado':{'inseto':{'hematofago':'pulga','herbivoro':'lagarta'},
+                   'anelideo':{'hematofago':'sanguessuga','onivoro':'minhoca'}}}
+a=input()
+b=input()
+c=input()
+print(l[a][b][c])
