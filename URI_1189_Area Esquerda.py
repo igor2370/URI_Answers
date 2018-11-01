f207ab88b414df2f9ca143c802490fe31aac6516
Jorge Igor da Silva Albuquerque <igor2370@hotmail.com>
2018-11-01 14:03:57 -0300

    Update URI_1189_Area Esquerda.py

---
+++ URI_1189_Area Esquerda.py
@@ -0,0 +1,24 @@
+O=input()
+M = []
+for i in range(12):
+    N = []
+    for j in range(12):
+        N.append(float(input()))
+    M.append(N)
+soma = 0
+indice=1
+for i in range(1,11):     
+    for j in range(indice):
+        soma += M[i][j]
+    if i<5:
+        indice+=1
+    elif i==5:
+        continue
+    elif i>=6:
+        indice-=1
+
+media = soma/30
+if O == 'S':
+    print('{:.1f}'.format(soma))
+elif O == 'M':
+    print('{:.1f}'.format(media))
