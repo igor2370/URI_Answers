e345c2c4cfcdf494607b05e71a1ea565225e5e45
Jorge Igor da Silva Albuquerque <igor2370@hotmail.com>
2018-11-01 14:03:39 -0300

    Update URI_1183_Acima da Diagonal Principal.py

---
+++ URI_1183_Acima da Diagonal Principal.py
@@ -0,0 +1,20 @@
+T=input()
+M = []
+for i in range(12):
+    N = []
+    for j in range(12):
+        N.append(float(input()))
+    M.append(N)
+soma = 0
+indice=1
+for i in range(12):
+    for j in range(indice,12):
+        soma += M[i][j]
+    indice+=1
+    if indice==12:
+        break
+media = soma/66
+if T =='S':
+    print('{:.1f}'.format(soma))
+elif T=='M':
+    print('{:.1f}'.format(media))
